bf5affa6bf6aaa9601a12718711b77a050e50c61
willcutrim <willyam894@gmail.com>
2021-04-21 18:22:58 -0300

    Configuracao python

--- myconversor/urls.py
+++ myconversor/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from myconversor import views
 
 urlpatterns = [
-    path('home', views.ytb_down),
+    path('', views.ytb_down),
     path('download', views.yt_download),
 
 ]
