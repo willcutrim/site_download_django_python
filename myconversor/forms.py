c1314ea8a4b728527a19314215ce2ab146d087b7
willcutrim <willyam894@gmail.com>
2021-04-26 22:25:08 -0300

    segunda modificacao

--- myconversor/forms.py
+++ myconversor/forms.py
@@ -3,3 +3,4 @@ from django import forms
 
 class LinkForm(forms.Form):
     link = forms.CharField(max_length=155)
+
--- myconversor/views.py
+++ myconversor/views.py
@@ -5,6 +5,9 @@ from siteconversor.models import link
 from django.contrib import messages
 from pytube import YouTube
 import time
+from pathlib import Path
+from django.core.validators import URLValidator
+
 
 
 
@@ -13,25 +16,30 @@ def ytb_down(request):
     return render(request, 'html/ytb_main.html')
 
 
+
+    
+
 def yt_download(request):
     if request.method == "GET":
         url = request.GET.get('url')
-        obj = YouTube(url)
-        h = "Título:"
-        #resolucao = []
-        thumbnail = obj.thumbnail_url
-        titulo = obj.title
-        strm_all = obj.streams.filter()
-        alerta = "Download concluído..."
-        down = strm_all.first().download(r'C:\Users\willyam cutrim/Downloads')
-        return render(request, 'html/yt_download.html', {'thumbnail': thumbnail, 'titulo': titulo, 'h': h, 'strm_all': strm_all, 'down': down, 'alerta': alerta})
         
-#down': down,
-        
-        """for i in strm_all:
-            resolucao.append(i.resolution)
+        if url:
+            home = Path.home()
+            paht_video = home / 'Downloads'
+            url = request.GET.get('url')
+            obj = YouTube(url)
+            h = "Título:"
+            #resolucao = []
+            thumbnail = obj.thumbnail_url
+            titulo = obj.title
+            strm_all = obj.streams.filter()
+            alerta = "Download concluído..."
+            #down = strm_all.first().download(paht_video)
+        else:
+            return redirect('/')
             
-        resolucao = list(dict.fromkeys(resolucao))
-        resolucao.remove(None)
-        ordem = resolucao.sort(reverse=True)"""
+        return render(request, 'html/yt_download.html', {'thumbnail': thumbnail, 'titulo': titulo, 'h': h, 'strm_all': strm_all, 'alerta': alerta})
+    else:
+        render(request, 'html/ytb_main.html')
+        
 
